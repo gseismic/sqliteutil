f692eb6c8bd8c901057bf1bd534f6a7e741e08d2
Liu Shengli <liushengli203@163.com>
2020-10-15 08:46:10 +0800

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='sqliteutil',
     version='0.1.0',
     packages=find_packages(),
-    description='sqlite utils',
+    description='sqlite utilitis',
     author='Liu Shengli',
     author_email='liushengli203@163.com',
     zip_safe=False
